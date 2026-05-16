2910432f54e64851e8b69e0832a47579b0a9c862
Devendra <devendr.sattia@sasi.ac.in>
2026-05-16 19:41:06 +0530

    Deploy: Final configuration with untracked node_modules and H2 support

--- backend/src/main/java/com/civicpulse/backend/services/MailService.java
+++ backend/src/main/java/com/civicpulse/backend/services/MailService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
-
+// import org.springframework.mail.javamail.JavaMailSender;
 
 @Service
 public class MailService {
@@ -22,8 +22,8 @@ public class MailService {
             MimeMessage message = mailSender.createMimeMessage(); 
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
-            // helper.setTo(to);    - To Send Actual mail        
-            helper.setTo("civicpulse.official@gmail.com");
+            helper.setTo(to);   // - To Send Actual mail        
+            // helper.setTo("civicpulse.official@gmail.com");
             helper.setSubject(subject + to);
             helper.setText(htmlBody, true); // true = HTML
 
--- backend/src/main/java/com/civicpulse/backend/services/OtpService.java
+++ backend/src/main/java/com/civicpulse/backend/services/OtpService.java
@@ -2,9 +2,9 @@
 package com.civicpulse.backend.services;
 
 
-// import org.springframework.beans.factory.annotation.Autowired; - needed for actual mail feature
-// import org.springframework.mail.SimpleMailMessage; - Essential While Sending the actual mail otps since we are using console otps
-// import org.springframework.mail.javamail.JavaMailSender; - Essential for Actual OTPs
+import org.springframework.beans.factory.annotation.Autowired; //- needed for actual mail feature
+import org.springframework.mail.SimpleMailMessage;// - Essential While Sending the actual mail otps since we are using console otps
+import org.springframework.mail.javamail.JavaMailSender; //- Essential for Actual OTPs
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import java.util.Map;
@@ -15,8 +15,8 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class OtpService {
 
-    // @Autowired -- needed for actual mail feature
-    // private JavaMailSender mailSender; - Essential for Actual OTPs
+    @Autowired //-- needed for actual mail feature
+    private JavaMailSender mailSender; //- Essential for Actual OTPs
 
     // Stores: "user@email.com" -> "123456"
     private final Map<String, String> otpStorage = new ConcurrentHashMap<>();
@@ -48,26 +48,26 @@ public class OtpService {
         verifiedEmails.remove(email);
     }
 
-    //Actual Sending Logic
-    // private void sendEmail(String to, String otp) {
-    //     SimpleMailMessage message = new SimpleMailMessage();
-    //     message.setTo(to);
-    //     message.setSubject("CivicPulse Verification Code");
-    //     message.setText("Your verification code is: " + otp);
-    //     mailSender.send(message);
-    // }
+    // Actual Sending Logic
+    private void sendEmail(String to, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("CivicPulse Verification Code");
+        message.setText("Your verification code is: " + otp);
+        mailSender.send(message);
+    }
 
     //For Development Phase
-    private void sendEmail(String to, String otp) {
-        // --- DEV MODE: PRINT TO CONSOLE ---
-        System.out.println("========================================");
-        System.out.println(" DEVELOPMENT OTP FOR " + to + ": " + otp);
-        System.out.println("========================================");
-        // ----------------------------------
+    // private void sendEmail(String to, String otp) {
+    //     // // --- DEV MODE: PRINT TO CONSOLE ---
+        // System.out.println("========================================");
+        // System.out.println(" DEVELOPMENT OTP FOR " + to + ": " + otp);
+        // System.out.println("========================================");
+        // // ----------------------------------
 
-        // You can comment out the real email sending to save quota:
-        // javaMailSender.send(message); 
-    }
+    //     // You can comment out the real email sending to save quota:
+    //     javaMailSender.send(message); 
+    // }
     // Optional: Clean up memory every hour
     @Scheduled(fixedRate = 3600000)
     public void cleanup() {
